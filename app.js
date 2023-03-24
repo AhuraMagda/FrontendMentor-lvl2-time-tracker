9128a5eefd94d8efb578353037e8e37d1d16ca07
AhuraMagda <lubaszka.magda@gmail.com>
2023-03-24 09:28:08 +0100

    podmiana work

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 
 
+
+const workDailyHours = document.querySelector('#work-hours')
+
+
+// pobranie danych z innego pliku
+
 const url = './data.json'
+
+let workDataDaily;
 
 async function getData() {
     const response = await fetch(url);
     const data = await response.json();
     console.log(data)
-    console.log(data.)
+    workDataDaily = data[0].timeframes.daily.current
 }
 
-getData()
+
+// Podmiana godzin na te daily
+
+async function changeInnerData() {
+    await getData()
+    console.log(workDataDaily)
+    workDailyHours.innerHTML = workDataDaily;
+}
+
+changeInnerData()
