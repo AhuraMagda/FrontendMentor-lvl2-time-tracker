401b1a2c711bf5ea2981e92c83081b3dfac66b3d
AhuraMagda <lubaszka.magda@gmail.com>
2023-03-24 22:53:22 +0100

    shorter js

--- app.js
+++ app.js
@@ -13,72 +13,40 @@ const selfCareHours = document.querySelector('#self-care-hours')
 
 const url = './data.json'
 
-let workDataDaily;
-let playDataDaily;
-let studyDataDaily;
-let exerciseDataDaily;
-let socialDataDaily;
-let selfCareDataDaily;
-
 // Podmiana godzin na te daily
+
 
 async function getDailyData() {
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data)
-    workDataDaily = data[0].timeframes.daily.current
-    playDataDaily = data[1].timeframes.daily.current
-    studyDataDaily = data[2].timeframes.daily.current
-    exerciseDataDaily = data[3].timeframes.daily.current
-    socialDataDaily = data[4].timeframes.daily.current
-    selfCareDataDaily = data[5].timeframes.daily.current
-}
-
-let workDataWeekly;
-let playDataWeekly;
-let studyDataWeekly;
-let exerciseDataWeekly;
-let socialDataWeekly;
-let selfCareDataWeekly;
-
-async function getWeeklyData() {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data)
-    workDataWeekly = data[0].timeframes.weekly.current
-    playDataWeekly = data[1].timeframes.weekly.current
-    studyDataWeekly = data[2].timeframes.weekly.current
-    exerciseDataWeekly = data[3].timeframes.weekly.current
-    socialDataWeekly = data[4].timeframes.weekly.current
-    selfCareDataWeekly = data[5].timeframes.weekly.current
+    workHours.innerHTML = `${data[0].timeframes.daily.current} hrs`;
+    playHours.innerHTML = `${data[1].timeframes.daily.current} hrs`;
+    studyHours.innerHTML = `${data[2].timeframes.daily.current} hrs`;
+    exerciseHours.innerHTML = `${data[3].timeframes.daily.current} hrs`;
+    socialHours.innerHTML = `${data[4].timeframes.daily.current} hrs`;
+    selfCareHours.innerHTML = `${data[5].timeframes.daily.current} hrs`;
 }
 
 // Podmiana godzin na te weekly
 
-async function changeInnerDataDaily() {
-    await getDailyData()
-    workHours.innerHTML = `${workDataDaily} hrs`;
-    playHours.innerHTML = `${playDataDaily} hrs`;
-    studyHours.innerHTML = `${studyDataDaily} hrs`;
-    exerciseHours.innerHTML = `${exerciseDataDaily} hrs`;
-    socialHours.innerHTML = `${socialDataDaily} hrs`;
-    selfCareHours.innerHTML = `${selfCareDataDaily} hrs`;
+
+async function getWeeklyData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    workHours.innerHTML = `${data[0].timeframes.weekly.current} hrs`;
+    playHours.innerHTML = `${data[1].timeframes.weekly.current} hrs`;
+    studyHours.innerHTML = `${data[2].timeframes.weekly.current} hrs`;
+    exerciseHours.innerHTML = `${data[3].timeframes.weekly.current} hrs`;
+    socialHours.innerHTML = `${data[4].timeframes.weekly.current} hrs`;
+    selfCareHours.innerHTML = `${data[5].timeframes.weekly.current} hrs`;
 }
 
-async function changeInnerDataWeekly() {
-    await getWeeklyData()
-    workHours.innerHTML = `${workDataWeekly} hrs`;
-    playHours.innerHTML = `${playDataWeekly} hrs`;
-    studyHours.innerHTML = `${studyDataWeekly} hrs`;
-    exerciseHours.innerHTML = `${exerciseDataWeekly} hrs`;
-    socialHours.innerHTML = `${socialDataWeekly} hrs`;
-    selfCareHours.innerHTML = `${selfCareDataWeekly} hrs`;
-}
+// guziczki
 
 const dailyButton = document.querySelector("#daily-button");
 const weeklyButton = document.querySelector("#weekly-button");
 
-changeInnerDataDaily()
+getDailyData()
 
-dailyButton.addEventListener("click", changeInnerDataDaily)
-weeklyButton.addEventListener("click", changeInnerDataWeekly)
+dailyButton.addEventListener("click", getDailyData)
+weeklyButton.addEventListener("click", getWeeklyData)
